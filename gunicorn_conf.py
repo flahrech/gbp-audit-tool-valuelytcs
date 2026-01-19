529554d82ce58eff1a9bbab134cd39b6e5db1160
Fahd Lahrech <flahrech@mch.corp.int>
2026-01-19 00:37:45 -0500

    Fix OOM error by limiting gunicorn workers

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -5,8 +5,8 @@ bind = "0.0.0.0:8000"
 
 # Dynamically calculate the number of workers based on CPU cores
 # Formula: (2 * cores) + 1
-workers = (multiprocessing.cpu_count() * 2) + 1
-
+# workers = (multiprocessing.cpu_count() * 2) + 1
+workers = 2
 # Use the Uvicorn worker class for FastAPI compatibility
 worker_class = "uvicorn.workers.UvicornWorker"
 
